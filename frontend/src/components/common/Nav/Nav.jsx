19b58719e67106449ce145635d57a654453701e6
svyatoslavxq <svyatoslavxq@yandex.ru>
2022-12-19 11:38:02 +0300

    first refactoring

--- frontend/src/components/common/Nav/Nav.jsx
+++ frontend/src/components/common/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
 import { Navbar, Container, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useAuth } from '../../../contexts/AuthContext';
@@ -9,7 +10,7 @@ const Nav = () => {
   return (
     <Navbar className="shadow-sm navbar navbar-light">
       <Container className="container">
-        <a className="navbar-brand" href="/frontend/src/routes">Hexlet Chat</a>
+        <Link className="navbar-brand" to="/signup">Hexlet Chat</Link>
         {auth.loggedIn ? <Button onClick={() => auth.logOut()} className="btn-primary">{t('logOut')}</Button> : null}
       </Container>
     </Navbar>
